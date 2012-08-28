6e99143023c59f25b278408607211b68da6c267d
MaisaEngineering <contact@maisasolutions.com>
2012-08-28 16:29:06 +0530

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongidDemo::Application.config.secret_token = 'c3e30edf93f78b1e993d984c58ad398e5dc46c304f2a3ccdce5a9c642db48e3ac2719250962227a52b144e7df60e2aecaba85c419605d952540ca824625c1f63'
